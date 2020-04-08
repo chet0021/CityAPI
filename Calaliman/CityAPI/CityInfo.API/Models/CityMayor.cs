b57b06cf5009b7b94ecfb42dbc9ed16a1006cdf2
liching13 <dharyll.jan.calaliman013@gmail.com>
2020-04-08 16:12:37 +0800

    WebAPI Activity

---
+++ Calaliman/CityAPI/CityInfo.API/Models/CityMayor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Models
+{
+    public class CityMayor
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(200)]
+        public string MayorName { get; set; }
+
+        [Required(ErrorMessage = "Age is required")]
+        [Range(40, 100, ErrorMessage = "Underage being a mayor")]
+        public int Age { get; set; }
+    }
+}
