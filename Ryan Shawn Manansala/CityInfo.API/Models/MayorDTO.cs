5ec07d094c45c6cb018fb70ab9bb94130e772d05
Ryan Shawn Manansala <rmanansala@BLASTASIA.local>
2020-04-08 15:59:33 +0800

    added Ryan Folder

---
+++ Ryan Shawn Manansala/CityInfo.API/Models/MayorDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Models
+{
+    public class MayorDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Range(40,100,ErrorMessage ="Mayor is underage")]
+        public int Age { get; set; }
+    }
+}
